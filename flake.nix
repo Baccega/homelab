ee919c68daf2907f23f4edf87580230430e01072
Sandro Baccega <sandro.baccega@chax.at>
2025-05-15 15:05:09 +0200

    🚀 Add nixinate to deploy

--- flake.nix
+++ flake.nix
@@ -1,22 +1,39 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.disko.url = "github:nix-community/disko";
-  inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+    nixinate.url = "github:matthewcroughan/nixinate";
+  };
 
   outputs =
     {
+      self,
       nixpkgs,
+      nixinate,
       disko,
       ...
     }:
     {
-      nixosConfigurations.laika = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          disko.nixosModules.disko
-          ./hosts/laika/configuration.nix
-          ./hosts/laika/hardware-configuration.nix
-        ];
+      apps = nixinate.nixinate.aarch64-darwin self;
+      nixosConfigurations = {
+        laika = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            disko.nixosModules.disko
+            ./hosts/laika/configuration.nix
+            ./hosts/laika/hardware-configuration.nix
+            {
+              _module.args.nixinate = {
+                host = "192.168.1.112";
+                sshUser = "root";
+                buildOn = "remote";
+                substituteOnTarget = true;
+                hermetic = false;
+              };
+            }
+          ];
+        };
       };
     };
 }
