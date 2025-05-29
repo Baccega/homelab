076f93aa0519e31b55f85c4a8abe62f692096dc1
Sandro Baccega <sandro.baccega@chax.at>
2025-05-29 22:01:00 +0200

    🐟 Add fish shell

--- users/sandro.nix
+++ users/sandro.nix
@@ -7,6 +7,20 @@
     sops.secrets.sandro-password.neededForUsers = true;
     users.mutableUsers = false;
 
+    home-manager.users.sandro = {
+        programs.fish.enable = true;
+    };
+
+    programs.bash = {
+        interactiveShellInit = ''
+            if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+            then
+            shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+            exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+            fi
+        '';
+    };
+
     users.users.sandro = {
         isNormalUser = true;
         uid = 1000;
