2c6c2897ecfda9230cef593e3a18eabe28d6a971
Sandro Baccega <sandro.baccega@chax.at>
2025-06-03 22:42:23 +0200

    🏗️ Move fish to module

--- users/sandro.nix
+++ users/sandro.nix
@@ -5,24 +5,15 @@
 }:
 let
   constants = import ../constants.nix;
+  username = "sandro";
 in
 {
+    imports = [
+        (import ../modules/fish.nix { inherit config pkgs username; })
+    ];
+
     sops.secrets.sandro-password.neededForUsers = true;
     users.mutableUsers = false;
-
-    home-manager.users.sandro = {
-        programs.fish.enable = true;
-    };
-
-    programs.bash = {
-        interactiveShellInit = ''
-            if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-            then
-            shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-            exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-            fi
-        '';
-    };
 
     users.users.sandro = {
         isNormalUser = true;
@@ -31,7 +22,6 @@ in
             "wheel" "networkmanager" "docker"
         ];
         hashedPasswordFile = config.sops.secrets.sandro-password.path;
-        # shell = pkgs.fish;
         createHome = true;
         home = "/home/sandro";
 
